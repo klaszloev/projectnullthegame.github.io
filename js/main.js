fff5db02f5981f886258ee7c82cd6200f7a4cd3e
projectnullthegame <16080518+projectnullthegame@users.noreply.github.com>
2023-03-16 14:56:12 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function scrollTo(id){
     var tag = $("a[name='"+ id +"']");
     $('html,body').animate({scrollTop: tag.offset().top}, 'slow');
@@ -7,44 +6,3 @@ function scrollTo(id){
 $('#goto_up, #navmenu_goto_up').click(function() { $('html, body').animate({ scrollTop: 0 }, 500); });
 $("#goto_pictures, #navmenu_goto_pictures").click(function() { scrollTo('section_pictures'); });
 $(".goto_contact").click(function() { scrollTo('section_contact'); });
-
-function throttle(func, timeFrame) {
-    var lastTime = 0;
-    return function(...args) {
-        var now = new Date().getTime();
-        if (now - lastTime >= timeFrame) {
-            func(...args);
-            lastTime = now;
-        }
-    };
-}
-
-// get the theme color on load so we can revert to this
-const ogColor = document.querySelector('meta[name="theme-color"]')?.getAttribute('content');
-// find all tags that have `data-scroll as a property`
-const targets = document.querySelectorAll('[data-scroll-theme]')
-
-// handle scroll event
-const handleScroll = throttle(() => {
-    // are any targets at the top of the window?
-    const isTop = Array.from(targets).map((target) => {
-        const rect = target.getBoundingClientRect();
-        if (rect.y > 1) return null;
-        return { target, rect }
-    }).filter(Boolean).sort((a, b) => b.rect.y - a.rect.y)[0]
-    // if we found an element at the top of the document then
-    if (isTop) {
-        // set theme color meta tag to the background color of div
-        const color = window.getComputedStyle(isTop.target).getPropertyValue('background-color')
-        if (color) {
-            // find the theme color meta tag and set the attribute to it
-            document.querySelector('meta[name="theme-color"]')?.setAttribute('content', color);
-        }
-    } else if (ogColor) {
-        // set theme color meta tag to original
-        document.querySelector('meta[name="theme-color"]')?.setAttribute('content', ogColor);
-    }
-    // run every 10ms
-}, 10)
-
-document.addEventListener('scroll', handleScroll, { passive: true })
