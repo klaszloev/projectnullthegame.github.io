1d7b807180f59c3cce7a8e56aba0fbc0b7bcd51e
projectnullthegame <16080518+projectnullthegame@users.noreply.github.com>
2023-03-16 13:39:48 +0100

    New design
    + added more social links

--- js/main.js
+++ js/main.js
@@ -8,27 +8,6 @@ $('#goto_up, #navmenu_goto_up').click(function() { $('html, body').animate({ scr
 $("#goto_pictures, #navmenu_goto_pictures").click(function() { scrollTo('section_pictures'); });
 $(".goto_contact").click(function() { scrollTo('section_contact'); });
 
-$(document).ready(function() {
-    var $window = $(window);
-
-    function checkWidth() {
-        $("div[class^='ref']").each(function(i) {
-        if ($(this).attr('class').length == 4) {
-            $(this).children().hide();
-
-            $(this).mouseenter(function() {
-                $(this).children().fadeIn().css("display", "table");
-            }).mouseleave(function() {
-                $(this).children().fadeOut();
-            });
-        }
-        });   
-    }
-
-    checkWidth();
-    $(window).resize(checkWidth);
-});
-
 function throttle(func, timeFrame) {
     var lastTime = 0;
     return function(...args) {
